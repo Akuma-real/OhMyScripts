c409c9de2dd8c611b9f81da0de695bd9c9ee2b82
鬼鬼 <54203072+Akuma-real@users.noreply.github.com>
2024-06-06 19:49:43 +0800

    Update configure_ssh.sh

--- configure_ssh.sh
+++ configure_ssh.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 定义公钥变量
+PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCybZq+l2HyVoHSa45b0v07yNap5KgDMUsruivIFfuEZr/Dgfd28u6nEcKhvkBX439Ro/fX7PIn2Jvs09iu+xnkzkhrnnO2GMn7ZCuLJv/qaKZ4I1/3H5FVygbi4DPxof6eOMcb5CRdheih+TAELhHhlGIKrw/hqMQOKPEr/O5yd+2hj1+zqB1r8vTv8yWdRqK8NOosY1dzETMCcORF6W7E4MnY3Ae6lA+IBe7I8PDN4EchK7tnTj0ZNCeqWgeMeekQisF4EfKuuLue26usbz7C3RC5LUNkrzaBJarXekkANxnvT039e0JV20U3oN+cyh+LvNgZ2UnFrqeR7G/cBkwZ"
+
 # 确保 ~/.ssh 目录存在
 if [ ! -d ~/.ssh ]; then
   mkdir ~/.ssh
@@ -7,7 +10,7 @@ if [ ! -d ~/.ssh ]; then
 fi
 
 # 将公钥添加到 ~/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCybZq+l2HyVoHSa45b0v07yNap5KgDMUsruivIFfuEZr/Dgfd28u6nEcKhvkBX439Ro/fX7PIn2Jvs09iu+xnkzkhrnnO2GMn7ZCuLJv/qaKZ4I1/3H5FVygbi4DPxof6eOMcb5CRdheih+TAELhHhlGIKrw/hqMQOKPEr/O5yd+2hj1+zqB1r8vTv8yWdRqK8NOosY1dzETMCcORF6W7E4MnY3Ae6lA+IBe7I8PDN4EchK7tnTj0ZNCeqWgeMeekQisF4EfKuuLue26usbz7C3RC5LUNkrzaBJarXekkANxnvT039e0JV20U3oN+cyh+LvNgZ2UnFrqeR7G/cBkwZ" >> ~/.ssh/authorized_keys
+echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
 # 启用公钥认证
